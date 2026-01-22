eaf4853ec7c21c868e68b40a283e7f5e9f5992da
mishelld <mishelldub@gmail.com>
2026-01-22 11:43:15 +0200

    Implement predictWords function

--- AutoCompleteTrie.js
+++ AutoCompleteTrie.js
@@ -32,12 +32,42 @@ class AutoCompleteTrie {
     }
     return true;
   }
+  predictWords(prefix) {
+    let allWords = [];
+    let node = this._getRemainingTree(prefix, this);
+    this._allWordsHelper(prefix, node, allWords);
+    return allWords;
+  }
+  _getRemainingTree(prefix, node) {
+    let i = 0;
+    while (i < prefix.length) {
+      node = node.children[prefix[i]];
+      i++;
+    }
+    return node;
+  }
+  _allWordsHelper(prefix, node, allWords) {
+    if (node.endOfWord) {
+      allWords.push(prefix);
+    }
+    for (let key of Object.keys(node.children)) {
+      const child = node.children[key];
+      this._allWordsHelper(prefix + child.value, child, allWords);
+    }
+  }
+  print(node = this) {
+    console.log(node.value, node.endOfWord);
+    for (let key of Object.keys(node.children)) {
+      this.print(node.children[key]);
+    }
+  }
 }
 let trie = new AutoCompleteTrie();
 trie.addWord("run");
 trie.addWord("rick");
 
 trie.addWord("running");
-console.log(trie.findWord("rick"));
+//console.log(trie.findWord("rick"));
+console.log(trie.predictWords("r"));
 
 //trie.print();
